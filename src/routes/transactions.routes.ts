9517ce3284d161912378c7143fc803b7a7ea9e59
Sam Marxz <sammarxzz@gmail.com>
2020-12-20 15:40:04 -0300

    add create transaction route.

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import ListTransactionsService from '../services/ListTransactionsService';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionsRouter = Router();
 
@@ -10,6 +11,21 @@ transactionsRouter.get('/', async (request, response) => {
   const list = await listTransactions.execute();
 
   return response.status(200).json(list);
+});
+
+transactionsRouter.post('/', async (request, response) => {
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 export default transactionsRouter;
