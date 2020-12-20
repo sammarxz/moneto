52412800e729dad8a1cd3be8aa30dad75e6b6ece
Sam Marxz <sammarxzz@gmail.com>
2020-12-20 15:49:23 -0300

    Delete Transaction.

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import ListTransactionsService from '../services/ListTransactionsService';
 import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 
 const transactionsRouter = Router();
 
@@ -26,6 +27,16 @@ transactionsRouter.post('/', async (request, response) => {
   });
 
   return response.json(transaction);
+});
+
+transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute({ id });
+
+  return response.status(200).send();
 });
 
 export default transactionsRouter;
---
+++ src/services/DeleteTransactionService.ts
@@ -0,0 +1,25 @@
+import { getCustomRepository } from 'typeorm';
+
+import TransationRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
+
+interface Request {
+  id: string;
+}
+
+class DeleteTransactionService {
+  public async execute({ id }: Request): Promise<void> {
+    const transationRepository = getCustomRepository(TransationRepository);
+
+    const transaction = transationRepository.findOne({ where: { id } });
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exists');
+    }
+
+    await transationRepository.delete({ id });
+  }
+}
+
+export default DeleteTransactionService;
