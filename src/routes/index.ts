71eae98652e6e78cff9faecc51c0daf302ba8a37
Sam Marxz <sammarxzz@gmail.com>
2020-12-20 12:20:06 -0300

    Add VSCODE Debug configs.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,4 +4,15 @@ const routes = Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Hello Moneto' }));
 
+routes.post('/', (req, res) => {
+  const { name, email } = req.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return res.json(user);
+});
+
 export default routes;
