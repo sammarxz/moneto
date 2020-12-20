c68c9b4849c368784e0e4e5747757a5c5c9db729
Sam Marxz <sammarxzz@gmail.com>
2020-12-20 15:14:13 -0300

    Create Transaction Service.

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,49 @@
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
+interface Response {
+  id: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Invalid type');
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
